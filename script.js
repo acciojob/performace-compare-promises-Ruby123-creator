e352afa19741640cbada773e78b529df30e7dd3b
Ruby pal <rubyp20001@gmail.com>
2023-04-05 15:00:43 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,45 +1,46 @@
 // Array of API URLs to fetch data from
-const apiUrls = [
-	"https://jsonplaceholder.typicode.com/todos/1",
-	"https://jsonplaceholder.typicode.com/todos/2",
-	"https://jsonplaceholder.typicode.com/todos/3",
-	"https://jsonplaceholder.typicode.com/todos/4",
-	"https://jsonplaceholder.typicode.com/todos/5",
-	"https://jsonplaceholder.typicode.com/todos/6",
-	"https://jsonplaceholder.typicode.com/todos/7",
-	"https://jsonplaceholder.typicode.com/todos/8",
-	"https://jsonplaceholder.typicode.com/todos/9",
-	"https://jsonplaceholder.typicode.com/todos/10",
-  ];
-  
-  // You can write your code here
-  let time1 = document.getElementById("output-all")
-  let time2 = document.getElementById("output-any")
- 
-		let st = new Date();
-	Promise.any(apiUrls).then((data)=>{
-		console.log(data)
-		
-			fetch(data).then((response)=>response.json())
-                
-        let ft = new Date();
-		time2.innerText = ft-st;
-		console.log(ft-st);
-			
-		
-	})
+const urls = [
+  "https://jsonplaceholder.typicode.com/todos/1",
+  "https://jsonplaceholder.typicode.com/todos/2",
+  "https://jsonplaceholder.typicode.com/todos/3",
+  "https://jsonplaceholder.typicode.com/todos/4",
+  "https://jsonplaceholder.typicode.com/todos/5",
+  "https://jsonplaceholder.typicode.com/todos/6",
+  "https://jsonplaceholder.typicode.com/todos/7",
+  "https://jsonplaceholder.typicode.com/todos/8",
+  "https://jsonplaceholder.typicode.com/todos/9",
+  "https://jsonplaceholder.typicode.com/todos/10",
+];
 
-	Promise.all(apiUrls).then((data)=>{
-		console.log(data)
-		
-		data.forEach((element)=>{
-			fetch(element).then((response)=>response.json())
+// You can write your code 
+function fetchWithPromiseAll(urls) {
+  const start = Date.now();
+  return Promise.all(urls.map(url => fetch(url)))
+    .then(responses => Promise.all(responses.map(response => response.json())))
+    .then(data => {
+      const time = Date.now() - start;
+      const outputAll = document.getElementById('output-all');
+      outputAll.innerText = `Promise.all took ${time}ms to fetch data from ${urls.length} APIs`;
+      return data;
+    })
+    .catch(error => console.error(error));
+}
 
-		})
-                
-        let ft = new Date();
-		time1.innerText = ft-st;
-		console.log(ft-st);
-			
-		
-	})
+function fetchWithPromiseAny(urls) {
+  const start = Date.now();
+  return Promise.any(urls.map(url => fetch(url)))
+    .then(response => response.json())
+    .then(data => {
+      const time = Date.now() - start;
+      const outputAny = document.getElementById('output-any');
+      outputAny.innerText = `Promise.any took ${time}ms to fetch data from ${urls.length} APIs`;
+      return data;
+    })
+    .catch(error => console.error(error));
+}
+
+Promise.all([
+  fetchWithPromiseAll(urls),
+  fetchWithPromiseAny(urls)
+]).then(results => console.log(results))
+  .catch(error => console.error(error));
